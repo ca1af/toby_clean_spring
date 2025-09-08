07c07311327be74d86d27b2bda8de87427321ce6
poby <laancer4@gmail.com>
2025-09-08 13:10:10 +0900

    init

---
+++ src/test/kotlin/com/calaf/cleanspring/CleanSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.calaf.cleanspring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CleanSpringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
